96a31cb5d9d2d6af946f7da9b435448735f8511c
pmalhaire <pierre.malhaire@gmail.com>
2020-05-01 14:11:01 +0200

    begin the file split

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 #include <GL/glut.h>
 #include <math.h>
 #include <time.h>
-#include <FreeImage.h>
 #include <functional>
+#include "textures.hpp"
 
 static GLfloat angleMoon = 0.0, angleEarth = 0.0, angleAstroid = 0.0,
                angleMars = 0.0, angleMercury = 0.0, angleVenus = 0.0,
@@ -317,74 +317,6 @@ static void reshape(int w, int h) {
   g_height = h;
 }
 
-FIBITMAP *loadImage(const char *filename) {
-  FIBITMAP *dib1 = NULL;
-
-  FREE_IMAGE_FORMAT fif = FreeImage_GetFIFFromFilename(filename);
-
-  dib1 = FreeImage_Load(fif, filename, JPEG_DEFAULT);
-  if (!dib1) {
-    exit(1);
-  }
-  return dib1;
-}
-
-GLuint loadTexture(FIBITMAP *dib1) {
-  GLuint tex_id = 0;
-  int x, y;
-  int height, width;
-
-  RGBQUAD rgbquad;
-
-  FREE_IMAGE_TYPE type;
-  BITMAPINFOHEADER *header;
-
-  type = FreeImage_GetImageType(dib1);
-
-  height = FreeImage_GetHeight(dib1);
-  width = FreeImage_GetWidth(dib1);
-
-  header = FreeImage_GetInfoHeader(dib1);
-  int scanLineWidh =
-      ((3 * width) % 4 == 0) ? 3 * width : ((3 * width) / 4) * 4 + 4;
-  unsigned char *texels =
-      (GLubyte *)calloc(height * scanLineWidh, sizeof(GLubyte));
-  for (x = 0; x < width; x++)
-    for (y = 0; y < height; y++) {
-      FreeImage_GetPixelColor(dib1, x, y, &rgbquad);
-      texels[(y * scanLineWidh + 3 * x)] = ((GLubyte *)&rgbquad)[2];
-      texels[(y * scanLineWidh + 3 * x) + 1] = ((GLubyte *)&rgbquad)[1];
-      texels[(y * scanLineWidh + 3 * x) + 2] = ((GLubyte *)&rgbquad)[0];
-    }
-
-  glGenTextures(1, &tex_id);
-  glBindTexture(GL_TEXTURE_2D, tex_id);
-
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
-               GL_UNSIGNED_BYTE, texels);
-
-  free(texels);
-
-  return tex_id;
-}
-void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id,
-                                GLenum severity, GLsizei length,
-                                const GLchar *message, const void *userParam) {
-  fprintf(stderr,
-          "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
-          (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type, severity,
-          message);
-}
-
-static void load_texture(const char *path, GLuint *idx) {
-  FIBITMAP *img = loadImage(path);
-  *idx = loadTexture(img);
-  FreeImage_Unload(img);
-}
-
 static void load_textures() {
   load_texture("images/sun.jpg", &sun_tex);
   load_texture("images/moon.jpg", &moon_tex);
@@ -396,6 +328,15 @@ static void load_textures() {
   load_texture("images/saturn.jpg", &saturn_tex);
   load_texture("images/uranus.jpg", &uranus_tex);
   load_texture("images/neptune.jpg", &neptune_tex);
+}
+
+void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id,
+                                GLenum severity, GLsizei length,
+                                const GLchar *message, const void *userParam) {
+  fprintf(stderr,
+          "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+          (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type, severity,
+          message);
 }
 
 int main(int argc, char **argv) {
---
+++ textures.hpp
@@ -0,0 +1,6 @@
+#ifndef TEXTURES_HPP
+#define TEXTURES_HPP
+#include <FreeImage.h>
+#include <GL/glut.h>
+void load_texture(const char *path, GLuint *idx);
+#endif
