4c3b832b318d16f7eb6dae1cd2dbfbd8359d8eab
pmalhaire <pierre.malhaire@gmail.com>
2020-05-10 18:19:47 +0200

    use absolute pos

--- planets.cpp
+++ planets.cpp
@@ -8,29 +8,34 @@
 #include <functional>
 #include "textures.hpp"
 
+struct position {
+  GLdouble x, y, z;
+};
+
 struct planet {
   GLfloat dist;
   GLdouble size;
-  GLfloat angle;
   GLfloat angleStep;
   GLuint tex;
+  struct position pos;
 };
 
-static std::map<const std::string, struct planet> planets{
-    {"sun", {0.0f, 0.2, 0.0f, 0}},
-    {"mercury", {0.295f, 0.016, 0.0, 2.0f, 0}},
-    {"venus", {0.4f, 0.02, 0.0f, 0.9f, 0}},
-    {"earth", {0.5f, 0.046, 0.0f, 0.7f, 0}},
-    {"earthMoon", {0.0598f, 0.023, 0.0f, 2.0f, 0}},
-    {"mars", {0.6f, 0.034, 0.0f, 0.5f, 0}},
-    {"jupiter", {0.8f, 0.1, 0.0f, 0.2f, 0}},
-    {"jupiterMoon", {0.11f, 0.005, 0.0f, 0.2f, 0}},
-    {"saturn", {1.0f, 0.08, 0.0f, 0.1f, 0}},
-    {"uranus", {1.05f, 0.046, 0.0f, 0.05f, 0}},
-    {"neptune", {1.13f, 0.04, 0.0f, 0.02f, 0}}};
+static int step = 0;
 
-static GLfloat sizeSaturnRing = 0.16f;
-static GLfloat angleAsteroid = 0.0f;
+static std::map<const std::string, struct planet> planets{
+    {"sun", {0.0f, 0.2, 0.0, 0}},
+    {"mercury", {0.295f, 0.016, 0.20f, 0}},
+    {"venus", {0.4f, 0.02, 0.09f, 0}},
+    {"earth", {0.5f, 0.046, 0.07f, 0}},
+    {"earthMoon", {0.0598f, 0.023, 0.20f, 0}},
+    {"mars", {0.6f, 0.034, 0.05f, 0}},
+    {"jupiter", {0.8f, 0.1, 0.02f, 0}},
+    {"jupiterMoon", {0.11f, 0.005, 0.2f, 0}},
+    {"saturn", {1.0f, 0.08, 0.01f, 0}},
+    {"uranus", {1.05f, 0.046, 0.005f, 0}},
+    {"neptune", {1.13f, 0.04, 0.002f, 0}}};
+
+static GLfloat sizeSaturnRing = 0.14f;
 
 static void push_pop(std::function<void()> action) {
   glPushMatrix();
@@ -55,7 +60,6 @@ static void orbit(GLfloat scale) {
   push_pop([scale](void) {
     glColor3f(0.3f, 0.3f, 0.3f);
     glEnable(GL_LINE_SMOOTH);
-    glRotatef(63, 1.0, 0.0, 0.0);
     glScalef(scale, scale, scale);
     glBegin(GL_LINE_LOOP);
 
@@ -83,29 +87,25 @@ static void create_sphere(GLdouble radius, GLint slice, GLint stacks,
 static void planet(const struct planet &p) {
   orbit(p.dist);
   push_pop([&p](void) {
-    glRotatef(p.angle, 0.0, 1.0, -0.5);
-    glTranslatef(p.dist, 0.0, 0.0);
+    glTranslatef(p.pos.x, p.pos.y, p.pos.z);
     create_sphere(p.size, 50, 50, p.tex);
   });
 }
 
 static void planet_moon(const struct planet &p, const struct planet &moon) {
   push_pop([&p, &moon](void) {
-    glRotatef(p.angle, 0.0, 1.0, -0.5);
-    glTranslatef(p.dist, 0.0, 0.0);
+    glTranslatef(p.pos.x, p.pos.y, p.pos.z);
     planet(moon);
   });
 }
 
 static void planet_ring(const struct planet &p, GLfloat &sizeRing) {
   push_pop([&p, &sizeRing](void) {
-    glRotatef(p.angle, 0.0, 1.0, -0.5);
-    glTranslatef(p.dist, 0.0, 0.0);
+    glTranslatef(p.pos.x, p.pos.y, p.pos.z);
     push_pop([&p, &sizeRing](void) {
       glEnable(GL_BLEND);
       glEnable(GL_COLOR_MATERIAL);
       glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-      glRotatef(45.0f, 1.0f, 0.0f, 0.0f);
       glScalef(sizeRing, sizeRing, sizeRing);
       glBegin(GL_TRIANGLE_FAN);
       const float alpha = 0.2f;
@@ -124,27 +124,19 @@ static void planet_ring(const struct planet &p, GLfloat &sizeRing) {
   });
 }
 
-static void update_angle(std::pair<const std::string, struct planet> &p) {
-  p.second.angle += p.second.angleStep;
-  if (p.second.angle > 360.0f) {
-    p.second.angle -= 360.0f;
-  }
-  if (p.second.angle < 0.0f) {
-    p.second.angle += 360.0f;
-  }
+static void update_pos(std::pair<const std::string, struct planet> &p) {
+  p.second.pos = {p.second.dist * cos(step * p.second.angleStep),
+                  p.second.dist * sin(step * p.second.angleStep), 0.0};
 }
 
 static void update_planets() {
-  std::for_each(planets.begin(), planets.end(), update_angle);
-
-  angleAsteroid += 0.002f;
-  if (angleAsteroid > 360.0f) {
-    angleAsteroid -= 360.0f;
+  ++step;
+  if (step > INT32_MAX) {
+    step = 0;
   }
-  if (angleAsteroid < 0.0f) {
-    angleAsteroid += 360.0f;
-  }
+  std::for_each(planets.begin(), planets.end(), update_pos);
 }
+
 static void
 load_planet_texture(std::pair<const std::string, struct planet> &p) {
   load_texture("images/" + p.first + ".jpg", &p.second.tex);
